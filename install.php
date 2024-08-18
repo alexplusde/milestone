d7a13df284be8693665f2591150ebe0cc0411890
Alexander Walther <mail@alexplus.de>
2024-08-18 20:07:31 +0200

    rex_sql_table bei der Installation verwenden, Index erstellen

--- install.php
+++ install.php
@@ -1,30 +1,26 @@
 <?php
 
+/* Tabelle anlegen und Index setzen */
+rex_sql_table::get(rex::getTable('milestone'))
+    ->ensurePrimaryIdColumn()
+    ->ensureColumn(new rex_sql_column('title', 'varchar(191)', false, ''))
+    ->ensureColumn(new rex_sql_column('description', 'text'))
+    ->ensureColumn(new rex_sql_column('image', 'text'))
+    ->ensureColumn(new rex_sql_column('date', 'date'))
+    ->ensureColumn(new rex_sql_column('status', 'tinyint(1)'))
+    ->ensureColumn(new rex_sql_column('createdate', 'datetime'))
+    ->ensureColumn(new rex_sql_column('createuser', 'varchar(191)'))
+    ->ensureColumn(new rex_sql_column('updatedate', 'datetime'))
+    ->ensureColumn(new rex_sql_column('updateuser', 'varchar(191)'))
+    ->ensureColumn(new rex_sql_column('uuid', 'varchar(36)'))
+    ->ensureIndex(new rex_sql_index('uuid', ['uuid']))
+    ->ensureIndex(new rex_sql_index('status', ['status']))
+    ->ensureIndex(new rex_sql_index('date', ['date']))
+    ->ensure();
+
 /* Tablesets aktualisieren */
 
 if (rex_addon::get('yform')->isAvailable() && !rex::isSafeMode()) {
     rex_yform_manager_table_api::importTablesets(rex_file::get(rex_path::addon($this->name, 'install/rex_milestone.tableset.json')));
     rex_yform_manager_table::deleteCache();
 }
-
-/*
-$modules = scandir(rex_path::addon("milestone")."module");
-
-foreach ($modules as $module) {
-    if ($module == "." || $module == "..") {
-        continue;
-    }
-    $module_array = json_decode(rex_file::get(rex_path::addon("milestone")."module/".$module), 1);
-
-    rex_sql::factory()->setDebug(0)->setTable("rex_module")
-    ->setValue("name", $module_array['name'])
-    ->setValue("key", $module_array['key'])
-    ->setValue("input", $module_array['input'])
-    ->setValue("output", $module_array['output'])
-    ->setValue("createuser", "")
-    ->setValue("updateuser", "milestone")
-    ->setValue("createdate", date("Y-m-d H:i:s"))
-    ->setValue("updatedate", date("Y-m-d H:i:s"))
-    ->insertOrUpdate();
-}
-*/
